ff0b5a584d69ed90bb7540fb077e4c0a58f4d2d5
Eric Schmidt <eric.schmidt@contentful.com>
2022-09-15 14:51:20 -0600

    Updates logic to properly work with multiple locales.

--- src/locations/Field.jsx
+++ src/locations/Field.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { TextInput } from '@contentful/f36-components';
+import React, { useEffect } from 'react';
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
+import { SingleLineEditor } from '@contentful/field-editor-single-line';
 
 const Field = () => {
   // Init the SDK.
@@ -10,37 +10,44 @@ const Field = () => {
   // text: "A tokenized pattern of field IDs on this content type that will be used
   // for replacement. Please wrap all tokens in square brackets (e.g. [fieldId])."
   const replacementPattern = sdk.parameters.instance.replacementPattern;
-  const [replacementString, setReplacementString] = useState(replacementPattern);
-
-  // Init replacementMap, which is used to store token to value mappings.
-  const replacementMap = {};
   // Get tokens from replacement pattern.
   const tokens = replacementPattern.match(/\[.*?\]/g);
+
+  // Initialize a map to contain all locales, which will then be populated with
+  // tokens and replacement strings.
+  const replacementMap = [];
+  const availableLocales = sdk.locales.available;
+  availableLocales.forEach((locale) => {
+    replacementMap[locale] = {};
+  });
 
   const updateReplacementMap = () => {
     // Create an object containing initial tokens and matched replaced strings.
     // A manual object is used because Object.entries doesn't seem to work with Maps.
     tokens.forEach((token) => {
       // Remove square brackets from field name.
-      const fieldName = token.slice(1, -1);
-      // Add actual field value to map array, keyed off of original token.
-      // We then use this later to replace each token with its field value.
-      replacementMap[token] = sdk.entry.fields[fieldName].getValue();
+      const fieldId = token.slice(1, -1);
+      // For each of the available locales, create an object to hold that
+      // locale's tokens and replacement values.
+      availableLocales.forEach((locale) => {
+        replacementMap[locale][token] = sdk.entry.fields[fieldId].getValue(locale);
+      });
     });
   };
 
-  const updateValue = () => {
-    // Replace tokens in replacement pattern for use in programmatically populated field.
-    Object.entries(replacementMap).forEach(([key, value]) => {
-      const fieldName = key.slice(1, -1);
-      sdk.entry.fields[fieldName].onValueChanged(() => {
-        // Update replacementMap values to reflect current input values.
-        updateReplacementMap();
-        // Update replacementString state variable and properly set the
-        // field value for this field to the replacement string value.
-        const updatedValue = replaceAll(replacementPattern, replacementMap);
-        setReplacementString(updatedValue);
-        sdk.field.setValue(updatedValue);
+  const updateFieldValues = () => {
+    Object.entries(replacementMap).forEach(([locale, tokens]) => {
+      Object.entries(tokens).forEach(([token, value]) => {
+        const fieldId = token.slice(1, -1);
+        sdk.entry.fields[fieldId].onValueChanged(locale, () => {
+          updateReplacementMap();
+
+          availableLocales.forEach((locale) => {
+            // TODO: Figure out why using `sdk.field.setValue` doesn't work with locale,
+            // instead it sets the value for ALL locales.
+            sdk.entry.fields[sdk.field.id].setValue(replaceAll(replacementPattern, replacementMap[locale]), locale);
+          });
+        });
       });
     });
   };
@@ -52,7 +59,6 @@ const Field = () => {
       return e.replace('[', '\\[').replace(']', '\\]');
     });
     const regex = new RegExp(escapedObjKeys.join('|'), 'g');
-
     return str.replace(regex, (matched) => {
       return mapObj[matched];
     });
@@ -64,7 +70,7 @@ const Field = () => {
 
   useEffect(() => {
     updateReplacementMap();
-    updateValue();
+    updateFieldValues();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   /*
@@ -76,7 +82,8 @@ const Field = () => {
   // reuse Contentful's editor components
   // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
 
-  return <TextInput name='internalTitle' type='text' value={replacementString} isDisabled={true} />;
+  // TODO: Figure out how to properly disable this.
+  return <SingleLineEditor field={sdk.field} locales={sdk.locales} isInitiallyDisabled={true} />;
 };
 
 export default Field;
